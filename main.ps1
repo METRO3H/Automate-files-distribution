cd397cfda99236944137387c4d39f29fa8a506b1
METROH <alejandrodesss@gmail.com>
2023-07-07 20:01:35 -0400

    Code optimization and improved terminal outputs

--- main.ps1
+++ main.ps1
@@ -8,50 +8,44 @@ $watcher.Filter = "*.*"
 $watcher.IncludeSubdirectories = $false
 $watcher.EnableRaisingEvents = $true
 
+$File_Extensions = @{
+    #Videos
+    ".mp4" = "Videos" ; ".mkv" = "Videos"
+    #Music
+    ".mp3" = "Music" ; ".wav" = "Music"; ".flac" = "Music"; ".m4a" = "Music"   
+    #Images
+    ".png" = "Images" ; ".webp" = "Images"; ".jpg" = "Images"; ".jpeg" = "Images"            
+    #Documents
+    ".txt" = "Documents\Text" ; ".pdf" = "Documents\PDF"; ".docx" = "Documents\Word"; ".pptx" = "Documents\Power point";
+    ".xlsx" = "Documents\Excel"
+    #Compressed
+    ".zip" = "Compressed"; ".rar" = "Compressed" ; ".tar" = "Compressed"; ".7z" = "Compressed"
+    #Setups
+    ".exe" = "Setups"
+    #Torrents
+    ".torrent" = "Torrents"                    
+}
 
 $action = {
     $file = $Event.SourceEventArgs.Name
     $fileFullPath = Join-Path -Path $downloadsPath -ChildPath $file
     $changeType = $Event.SourceEventArgs.ChangeType
+
     # Check if the event is for a file creation
-    if (-not($changeType -eq "Created") -or -not(Test-Path -Path $fileFullPath -PathType Leaf)) {
-        Write-Host "The triggered event is not caused by a file or the event is not 'created' type"
-        Write-Host "Event Type : " $changeType
-        Write-Host "Path : " $fileFullPath
+    if (-not(Test-Path -Path $fileFullPath -PathType Leaf)) {
+        Write-Host "The triggered event is not caused by a file. Event Path : "$fileFullPath
         return
     }
     $extension = [System.IO.Path]::GetExtension($fileFullPath)
 
-    $File_Extensions = @{
-        #Videos
-        ".mp4" = "Videos" ; ".mkv" = "Videos"
-        #Music
-        ".mp3" = "Music" ; ".wav" = "Music"; ".flac" = "Music"; ".m4a" = "Music"   
-        #Images
-        ".png" = "Images" ; ".webp" = "Images"; ".jpg" = "Images"; ".jpeg" = "Images"            
-        #Documents
-        ".txt" = "Documents\Text" ; ".pdf" = "Documents\PDF"; ".docx" = "Documents\Word"; ".pptx" = "Documents\Power point";
-        ".xlsx" = "Documents\Excel"
-        #Compressed
-        ".zip" = "Compressed"; ".rar" = "Compressed" ; ".tar" = "Compressed"; ".7z" = "Compressed"
-        #Setups
-        ".exe" = "Setups"
-        #Torrents
-        ".torrent" = "Torrents"                    
-    }
-
     $Type_Folder = ""
 
     if ($File_Extensions.ContainsKey($extension)) {
-        Write-Host "Case extension $extension found."
+
         $Type_Folder = $File_Extensions[$extension]
 
-<#          if($Type_Folder == "Torrents"){
-           return "F:\Torrents"
-       } #>
     }
     else {
-        Write-Host "Case not found."
         $Type_Folder = "Other"
     }
 
@@ -76,6 +70,7 @@ $action = {
                  
         try {
             Move-Item -Path $fileFullPath -Destination $destinationFileFullPath -Force
+            Write-Host "File '$file' moved successfully from '$fileFullPath' to '$destinationFileFullPath'"
         }
         catch [System.IO.IOException] {
             Write-Host "Sharing violation occurred while moving the file: $file"
